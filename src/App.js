bec7dd06ef292d68eb8c4c115e451b7db64b2f05
polina <korolkovapolina07@gmail.com>
2023-08-17 15:00:04 +0300

    added main functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,80 @@
 import "./App.css";
 import { Button, Stack } from "@mui/material";
-import CustomizedProgressBars from "./components/progressBar";
+import { BorderLinearProgress } from "./components/progressBar";
+import result from "./components/result";
+import { useState } from "react";
 
 function App() {
+  const questions = [
+    {
+      question: "What is the capital of Ukraine?",
+      answers: [
+        { btn: "Kyiv", correct: true },
+        { btn: "London", correct: false },
+        { btn: "Rome", correct: false },
+      ],
+    },
+    {
+      question: "How many continents are there?",
+      answers: [
+        { btn: "7", correct: true },
+        { btn: "9", correct: false },
+        { btn: "5", correct: false },
+      ],
+    },
+    {
+      question: "who is the author of the picture of dorian gray?",
+      answers: [
+        { btn: "Oscar Wilde", correct: true },
+        { btn: "Agatha Christie", correct: false },
+        { btn: "Jenny Han", correct: false },
+      ],
+    },
+    {
+      question: "how many countries are there in the uk?",
+      answers: [
+        { btn: "4", correct: true },
+        { btn: "6", correct: false },
+        { btn: "2", correct: false },
+      ],
+    },
+    {
+      question: "How many days are in a leap year?",
+      answers: [
+        { btn: "366", correct: true },
+        { btn: "365", correct: false },
+        { btn: "364", correct: false },
+      ],
+    },
+    {
+      question: "Congrats!you finished the test",
+    },
+  ];
+  let [score, setScore] = useState(0);
+  let [currentQuestion, setCurrentQuestion] = useState(0);
+  let [progress, setProgress] = useState(0);
+
+  const onClick = (e) => {
+    let text = e.target.id;
+    setProgress(progress + 20);
+    if (currentQuestion < 4) {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+    if (currentQuestion > 4) {
+    }
+    console.log(score);
+    if (questions[currentQuestion].answers[text].correct === true) {
+      console.log("correct");
+      setScore((score = score + 1));
+    } else {
+      console.log("wrong!");
+    }
+  };
+  const startAgain = () => {
+    setCurrentQuestion((currentQuestion = 0));
+    setScore((score = 0));
+    setProgress((progress = 0));
+  };
   return (
     <div className="body">
       <header className="App-header">
@@ -19,20 +91,23 @@ function App() {
             alignItems="center"
             spacing={1}
           >
-            <CustomizedProgressBars />
-            <h4 id="question">Quize</h4>
+            <BorderLinearProgress variant="determinate" value={progress} />
+            <h4 id="question">{questions[currentQuestion].question}</h4>
 
-            <button className="answer" id="answer" variant="outlined">
-              Var1
+            <button onClick={onClick} className="answer" id="0" type="button">
+              {questions[currentQuestion].answers[0].btn}
             </button>
-            <button className="answer" id="answer" variant="outlined">
-              Var2
+            <button onClick={onClick} className="answer" id="1" type="button">
+              {questions[currentQuestion].answers[1].btn}
             </button>
-            <button className="answer" id="answer" variant="outlined">
-              Var3
+            <button onClick={onClick} className="answer" id="2" type="button">
+              {questions[currentQuestion].answers[2].btn}
             </button>
           </Stack>
         </form>
+        <button onClick={startAgain} className="start" type="button">
+          Start again
+        </button>
       </header>
     </div>
   );
--- src/components/progressBar.jsx
+++ src/components/progressBar.jsx
@@ -8,7 +8,7 @@ import LinearProgress, {
   linearProgressClasses,
 } from "@mui/material/LinearProgress";
 
-const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+export const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 10,
   width: 500,
   borderRadius: 5,
@@ -21,51 +21,15 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "light" ? "red" : "#308fe8",
   },
 }));
-function FacebookCircularProgress(props) {
-  return (
-    <Box sx={{ position: "relative" }}>
-      <CircularProgress
-        variant="determinate"
-        sx={{
-          color: (theme) =>
-            theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
-        }}
-        size={40}
-        thickness={4}
-        {...props}
-        value={100}
-      />
-      <CircularProgress
-        variant="indeterminate"
-        disableShrink
-        sx={{
-          color: (theme) =>
-            theme.palette.mode === "light" ? "#1a90ff" : "#308fe8",
-          animationDuration: "550ms",
-          position: "absolute",
-          left: 0,
-          [`& .${circularProgressClasses.circle}`]: {
-            strokeLinecap: "round",
-          },
-        }}
-        size={40}
-        thickness={4}
-        {...props}
-      />
-    </Box>
-  );
-}
 
-export default function CustomizedProgressBars() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      {/* <FacebookCircularProgress /> */}
-
-      <BorderLinearProgress
-        variant="determinate"
-        value={10}
-        className="progress"
-      />
-    </Box>
-  );
-}
+// export default function CustomizedProgressBars() {
+//   return (
+//     <Box sx={{ flexGrow: 1 }}>
+//       <BorderLinearProgress
+//         variant="determinate"
+//         value={20}
+//         className="progress"
+//       />
+//     </Box>
+//   );
+// }
